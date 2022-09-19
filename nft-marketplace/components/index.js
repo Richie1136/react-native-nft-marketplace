bb9f3f09b0ef4d452018f45b43e7c58cd3fe57cf
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-19 13:46:36 -0400

    Created a index.js file inside of the components folder and exported all of my components inside of the index.js file

---
+++ nft-marketplace/components/index.js
@@ -0,0 +1,3 @@
+export { default as FocusedStatusBar } from './focusedstatusbar/FocusedStatusBar'
+export { default as HomeHeader } from './homeheader/HomeHeader'
+export { default as NFTCard } from './nftcard/NFTCard'

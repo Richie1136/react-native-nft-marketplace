b49c1c77522d1baa7da1e14750846bd3a5cc81eb
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-19 14:32:12 -0400

    Added styling to views and displaying the item names

--- nft-marketplace/screens/home/Home.js
+++ nft-marketplace/screens/home/Home.js
@@ -9,6 +9,19 @@ const Home = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar background={COLORS.primary} />
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList keyExtractor={(item) => item.id}
+            data={NFTData}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+            renderItem={({ item }) => <Text>{item.name}</Text>} />
+        </View>
+        <View style={{ position: 'absolute', top: 0, bottom: 0, right: 0, left: 0, zIndex: -1 }}>
+          <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+        </View>
+      </View>
     </SafeAreaView>
   )
 }

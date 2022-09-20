ae73875079e1e3e309d6588ce19d78d9008e1dcf
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-20 13:24:32 -0400

    Created functions inside of the Subinfo component and then imported those functions inside of the NFTCard component

--- nft-marketplace/components/nftcard/NFTCard.js
+++ nft-marketplace/components/nftcard/NFTCard.js
@@ -4,10 +4,10 @@ import { CircleButton } from '../circlebutton/CircleButton'
 
 import { COLORS, SIZES, SHADOWS, assets } from '../../screens/constants'
 
+import { SubInfo, People, NFTTitle, EthPrice, ImageCmp, EndDate } from '../subinfo/SubInfo'
+
 
 const NFTCard = ({ data }) => {
-
-  console.log(data)
 
   const navigation = useNavigation()
 
@@ -26,7 +26,7 @@ const NFTCard = ({ data }) => {
         <Image source={data.image} resizeMode="cover" style={{ width: '100%', height: '100%', borderTopLeftRadius: SIZES.font, borderTopRightRadius: SIZES.font }} />
         <CircleButton imgUrl={assets.heart} right={10} top={10} />
       </View>
-      <Text>NFTCard</Text>
+      <SubInfo />
     </View>
   )
 }
---
+++ nft-marketplace/components/subinfo/SubInfo.js
@@ -0,0 +1,49 @@
+import { View, Text } from 'react-native'
+
+
+export const NFTTitle = () => {
+  <View>
+    <Text>NFT Title</Text>
+  </View>
+}
+
+export const EthPrice = () => {
+  return (
+    <View>
+      <Text>Eth price</Text>
+    </View>
+  )
+}
+
+export const ImageCmp = () => {
+  return (
+    <View>
+      <Text>ImageCmp</Text>
+    </View>
+  )
+}
+
+export const People = () => {
+  return (
+    <View>
+      <Text>People</Text>
+    </View>
+  )
+}
+
+export const EndDate = () => {
+  return (
+    <View>
+      <Text>End Date</Text>
+    </View>
+  )
+}
+
+
+export const SubInfo = () => {
+  return (
+    <View>
+      <Text>SubInfo</Text>
+    </View>
+  )
+}

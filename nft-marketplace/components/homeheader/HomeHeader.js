dec7f2e25f45838cb8bff64e5fe36db033c013db
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-21 13:18:06 -0400

    Importing data from inside of the constants folder

--- nft-marketplace/components/homeheader/HomeHeader.js
+++ nft-marketplace/components/homeheader/HomeHeader.js
@@ -1,5 +1,7 @@
 import { View, Text, Image, TextInput } from 'react-native'
 
+import { COLORS, FONTS, SIZES, assets } from '../../screens/constants'
+
 const HomeHeader = () => {
   return (
     <View>

917a9457e16a4e52a8cbf9d58696e40095f13841
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-21 13:27:30 -0400

    Added 2 images inside of a view of a person and a verified badge and positioned them in the top right corner of the header

--- nft-marketplace/components/homeheader/HomeHeader.js
+++ nft-marketplace/components/homeheader/HomeHeader.js
@@ -4,8 +4,15 @@ import { COLORS, FONTS, SIZES, assets } from '../../screens/constants'
 
 const HomeHeader = () => {
   return (
-    <View>
-      <Text>HomeHeader</Text>
+    <View style={{ backgroundColor: COLORS.primary, padding: SIZES.font }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <Image source={assets.logo} resizeMode="contain" style={{ width: 90, height: 25 }} />
+        <View style={{ width: 45, height: 45 }}>
+          <Image source={assets.person01} resizeMode="contain" style={{ position: 'absolute', width: 15, height: 15, bottom: 0, right: 0 }} />
+          <Image source={assets.badge} resizeMode="contain" style={{ width: '100%', height: '100%' }} />
+
+        </View>
+      </View>
     </View>
   )
 }

4ed632d83bb94c9ad3715850915df7ade66aeb4a
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-21 14:42:35 -0400

    Added FlatList component and added styling and data.bids to show the amount of bids on the NFT

--- nft-marketplace/screens/details/Details.js
+++ nft-marketplace/screens/details/Details.js
@@ -15,6 +15,7 @@ const Details = ({ route, navigation }) => {
       <View style={{ width: '100%', position: 'absolute', bottom: 0, paddingVertical: SIZES.font, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(255,255,255,0.5)', zIndex: 1 }}>
         <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
       </View>
+      <FlatList keyExtractor={(item) => item.id} showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: SIZES.extraLarge * 3 }} data={data.bids} renderItem={({ item }) => <DetailsBid bid={item} />} />
     </SafeAreaView>
   )
 }

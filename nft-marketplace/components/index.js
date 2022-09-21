b718d27c1dc9538e939eb4a8e8a499fd3f795969
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-21 14:49:13 -0400

    Changed how i exported my components inside of the index.js file

--- nft-marketplace/components/index.js
+++ nft-marketplace/components/index.js
@@ -1,7 +1,22 @@
-export { default as FocusedStatusBar } from './focusedstatusbar/FocusedStatusBar'
-export { default as HomeHeader } from './homeheader/HomeHeader'
-export { default as NFTCard } from './nftcard/NFTCard'
-import { CircleButton } from './circlebutton/CircleButton'
-export { default as DetailsDescription } from './detailsdescription/DetailsDescription'
-export { default as DetailsBid } from './detailsbid/DetailsBid'
-export { CircleButton }
+import HomeHeader from "./homeheader/HomeHeader";
+import NFTCard from "./nftcard/NFTCard";
+import { RectButton, CircleButton } from "./circlebutton/CircleButton";
+import { SubInfo, People, EndDate, EthPrice, NFTTitle } from "./subinfo/SubInfo";
+import DetailsBid from "./detailsbid/DetailsBid";
+import FocusedStatusBar from "./focusedstatusbar/FocusedStatusBar";
+import DetailsDescription from "./detailsdescription/DetailsDescription";
+
+export {
+  HomeHeader,
+  NFTCard,
+  SubInfo,
+  People,
+  EndDate,
+  RectButton,
+  CircleButton,
+  EthPrice,
+  NFTTitle,
+  DetailsBid,
+  FocusedStatusBar,
+  DetailsDescription,
+};

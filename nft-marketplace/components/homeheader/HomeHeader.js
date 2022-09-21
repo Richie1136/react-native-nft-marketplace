8ea7fcadf0f54ecfca0467a0e1e5db9ce137df6d
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-21 13:29:16 -0400

    Fixed styling

--- nft-marketplace/components/homeheader/HomeHeader.js
+++ nft-marketplace/components/homeheader/HomeHeader.js
@@ -8,8 +8,8 @@ const HomeHeader = () => {
       <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
         <Image source={assets.logo} resizeMode="contain" style={{ width: 90, height: 25 }} />
         <View style={{ width: 45, height: 45 }}>
-          <Image source={assets.person01} resizeMode="contain" style={{ position: 'absolute', width: 15, height: 15, bottom: 0, right: 0 }} />
-          <Image source={assets.badge} resizeMode="contain" style={{ width: '100%', height: '100%' }} />
+          <Image source={assets.person01} resizeMode="contain" style={{ width: '100%', height: '100%' }} />
+          <Image source={assets.badge} resizeMode="contain" style={{ position: 'absolute', width: 15, height: 15, right: 0, bottom: 0 }} />
 
         </View>
       </View>

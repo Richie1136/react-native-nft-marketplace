722d4f56cf63d76d38b88655dbb8b6e4fe0966cf
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-22 13:30:57 -0400

    Added another view which holds the NFT description

--- nft-marketplace/components/detailsdescription/DetailsDescription.js
+++ nft-marketplace/components/detailsdescription/DetailsDescription.js
@@ -15,6 +15,12 @@ const DetailsDescription = ({ data }) => {
         <NFTTitle title={data.name} subTitle={data.creator} titleSize={SIZES.extraLarge} subTitleSize={SIZES.font} />
         <EthPrice price={data.price} />
       </View>
+      <View style={{ marginVertical: SIZES.extraLarge * 1.5 }}>
+        <Text style={{ fontSize: SIZES.font, fontFamily: FONTS.semiBold, color: COLORS.primary }}>Description</Text>
+        <View style={{ marginTop: SIZES.base }}>
+          <Text style={{ fontSize: SIZES.small, fontFamily: FONTS.regular, color: COLORS.secondary, lineHeight: SIZES.large }}>{data.description}</Text>
+        </View>
+      </View>
     </>
   )
 }

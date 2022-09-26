6e1ca65b6094f1261bf651ad83ace50224ca3275
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-26 13:16:47 -0400

    Added the date when the bid was placed and added styles to that Text

--- nft-marketplace/components/detailsbid/DetailsBid.js
+++ nft-marketplace/components/detailsbid/DetailsBid.js
@@ -10,6 +10,8 @@ const DetailsBid = ({ bid }) => {
       <Image source={bid.image} resizeMode="contain" style={{ width: 48, height: 48 }} />
       <View>
         <Text style={{ fontFamily: FONTS.semiBold, fontSize: SIZES.small, color: COLORS.primary }}>Bid placed by {bid.name}</Text>
+        <Text style={{ fontFamily: FONTS.regular, fontSize: SIZES.small - 2, color: COLORS.secondary, marginTop: 3 }}>{bid.date}</Text>
+
       </View>
     </View>
   )
